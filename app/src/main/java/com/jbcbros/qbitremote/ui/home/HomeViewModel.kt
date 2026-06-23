b8ba08e49d5e6c7e4649dfc4489919bebed60781
urbanescavenger <urbanescavenger@gmailcom>
2026-06-24 00:32:55 +0800

    fix: add missing kotlinx.coroutines.flow.firstOrNull import

--- app/src/main/java/com/jbcbros/qbitremote/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/jbcbros/qbitremote/ui/home/HomeViewModel.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import javax.inject.Inject
--- app/src/main/java/com/jbcbros/qbitremote/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/jbcbros/qbitremote/ui/settings/SettingsViewModel.kt
@@ -9,6 +9,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
